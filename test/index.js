4de3ad0e48573f84e228a4414bd38235859ecc52
Clément Pasquier <toxynell@gmail.com>
2016-08-17 20:59:05 +0200

    debug unit tests

--- test/index.js
+++ test/index.js
@@ -16,6 +16,27 @@ describe('Test nconf building', () => {
     done();
   });
 
+
+  it('Loading manifest', (done) => {
+
+    server.register({
+      register: require('../lib/index'),
+      options: {
+        defaults: [Path.join(__dirname, 'fixtures', 'default')],
+        paths: [Path.join(__dirname, 'fixtures', 'dev')]
+      }
+    }, (err) => {
+      expect(err).to.not.exist();
+      expect(server.bag).to.be.an.object();
+      console.log(server.bag.get('a:b:d'), server.bag.get('a:b:e'));
+      expect(server.bag.get('a:b:c')).to.be.true();
+      expect(server.bag.get('a:b:d')).to.be.false();
+      expect(server.bag.get('a:b:e')).to.be.true();
+      done();
+    });
+  });
+
+
   it('Loading manifest', (done) => {
     server.register({
       register: require('../lib/index'),
@@ -31,22 +52,4 @@ describe('Test nconf building', () => {
     });
   });
 
-  it('Loading manifest', (done) => {
-
-    server.register({
-      register: require('../lib/index'),
-      options: {
-        defaults: [Path.join(__dirname, 'fixtures', 'default')],
-        paths: [Path.join(__dirname, 'fixtures', 'dev')]
-      }
-    }, (err) => {
-      expect(err).to.not.exist()
-      expect(server.bag).to.be.an.object();
-      console.log(server.bag.get('a:b:d'), server.bag.get('a:b:e'));
-      expect(server.bag.get('a:b:c')).to.be.true();
-      expect(server.bag.get('a:b:d')).to.be.false();
-      expect(server.bag.get('a:b:e')).to.be.true();
-      done();
-    });
-  });
 });
