a7cf0301935a2fc27d0fabd6ae285718c2f43869
Nicolas Dufreche <n.dufreche@gmail.com>
2016-08-17 18:44:30 +0200

    use nconf and pellmell to generate configuration

---
+++ test/index.js
@@ -0,0 +1,52 @@
+'use strict';
+
+const Lab =  require('lab');
+const Hapi = require('hapi');
+const Path = require('path');
+const {expect} = require('code');
+const {describe, it, before, beforeEach} = exports.lab = Lab.script();
+
+describe('Test nconf building', () => {
+
+  let server;
+
+  beforeEach(done => {
+    server = new Hapi.Server();
+    server.connection();
+    done();
+  });
+
+  it('Loading manifest', (done) => {
+    server.register({
+      register: require('../lib/index'),
+      options: {
+        defaults: [Path.join(__dirname, 'fixtures', 'default')]
+      }
+    }, (err) => {
+      expect(err).to.not.exist()
+      expect(server.bag).to.be.an.object();
+      expect(server.bag.get('a:b:c')).to.be.true();
+      expect(server.bag.get('a:b:d')).to.be.true();
+      done();
+    });
+  });
+
+  it('Loading manifest', (done) => {
+
+    server.register({
+      register: require('../lib/index'),
+      options: {
+        defaults: [Path.join(__dirname, 'fixtures', 'default')],
+        paths: [Path.join(__dirname, 'fixtures', 'dev')]
+      }
+    }, (err) => {
+      expect(err).to.not.exist()
+      expect(server.bag).to.be.an.object();
+      console.log(server.bag.get('a:b:d'), server.bag.get('a:b:e'));
+      expect(server.bag.get('a:b:c')).to.be.true();
+      expect(server.bag.get('a:b:d')).to.be.false();
+      expect(server.bag.get('a:b:e')).to.be.true();
+      done();
+    });
+  });
+});
